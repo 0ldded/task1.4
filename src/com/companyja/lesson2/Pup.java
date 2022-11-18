f1b2f58ee1c7309273f01044905260d83e397836
0ldded <112765935+0ldded@users.noreply.github.com>
2022-11-18 21:59:52 +0300

    Initial commit

---
+++ src/com/companyja/lesson2/Pup.java
@@ -0,0 +1,12 @@
+package com.companyja.lesson2;
+
+public class Pup
+{
+    public static void main(String[] args)
+    {
+        for(String str : args)
+        {
+            System.out.println("Значение аргумента: " + str );
+        }
+    }
+}
